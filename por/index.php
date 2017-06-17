53063ba8d9b9d3c9232e6cedddfd61a3c7fc13e1
ThanwaTTY <to.tanwa@gmail.com>
2017-06-17 11:27:46 +0700

    test new

--- por/index.php
+++ por/index.php
@@ -2,12 +2,4 @@
 echo "hello Thanwa";
 echo "world";
 echo "pottest";
-<<<<<<< HEAD
 echo "Karun2";
-=======
-echo "palmsogood";
-echo "palm";
-echo "Edit TOR <BR>";
-
-
->>>>>>> bd425ced010125ef970ac3c38f8ea3ce3c229eb3
